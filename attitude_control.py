95821c0d781df365927a5e6e91c5f049ce39e694
TSUTAYA44 <tatuya7974@gmail.com>
2020-01-08 01:34:56 +0900

    '修正済み'

--- attitude_control.py
+++ attitude_control.py
@@ -5,49 +5,71 @@ from servo import ServoController
 from mpu6050 import Mpu6050
 
 
-#初期値の設定
+# 初期値の設定
 dt = 0
-total_time = 0
-old_y_gyro = 0
-old_angle = 0
-fieldnames = ['x_accel', 'y_accel', 'z_accel', 'x_gyro', 'y_gyro', 'z_gyro', 'dt']
-data_logs = []
-CALC_TIME = 20
+current_time = 0
+prev_gyro = 0
+prev_angle = 0
+fieldnames = ['accel_x', 'accel_y', 'accel_z',
+              'gyro_x', 'gyro_y', 'gyro_y', 'dt']
+instrumentation_logs = []
+CALC_TIME = 50
+
+
+def find_angle_in_acceleration(accel_x, accel_y, accel_z):
+    accel_only_angle = math.atan2(
+        accel_x, math.sqrt(accel_y ** 2 + accel_z ** 2))
+    return accel_only_angle
+
+
+def complement_filter_coefficient(accel_x):
+    factor = 0.2 * (65536 ** -(abs(accel_x) / 10))  # 加速度の大きさによって変化する係数
+    return factor
+
+
+def complement_filter(accel_only_angle, y_gyro, prev_angle, prev_gyro, factor):
+    real_angle = (1 - factor) * (prev_angle + math.radians((prev_gyro +
+                                                            y_gyro) * dt / 2)) + factor * accel_only_angle  # 相補フィルター
+    prev_angle = real_angle  # 角度の変化量を足していくため一つ前の角度が必要
+    prev_gyro = y_gyro  # 台形積分用に一つ前の角速度を残しておく
+    adjust_angle = 90 - int(math.degrees(real_angle))  # -90度~90度の範囲を0度~180度に変換
+    return [adjust_angle, prev_angle, prev_gyro]
+
 
 mpu6050 = Mpu6050()
 
-Servo = ServoController(Pin=18)
-Servo.set_position(90) #初期位置を90度に設定
+servo = ServoController(Pin=18)  # 物理ピン番号
+servo.set_position(90)  # 初期位置を90度に設定
 
 while 1:
-    start = time.time() #ループ中にかかる時間を計測開始
+    start = time.time()  # ループ中にかかる時間を計測開始
 
-    x_gyro,  y_gyro,  z_gyro  = mpu6050.gyro_lsb()
-    x_accel, y_accel, z_accel = mpu6050.accel_lsb()
+    gyro_x,  gyro_y,  gyro_z = mpu6050.gyro_lsb()
+    accel_x, accel_y, accel_z = mpu6050.accel_lsb()
 
-    data_logs.append([x_accel, y_accel, z_accel, x_gyro, y_gyro, z_gyro, dt])
+    instrumentation_logs.append(
+        [accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z, dt])
 
-    y_angle = math.atan2(x_accel , math.sqrt(y_accel**2 + z_accel**2))#姿勢角の算出
-    k = 0.2 * (65536**-(abs(x_accel)/10)) #加速度が増加→加速度から得られる角度の比重を小さくしていく係数k
-    y_angle = (((1-k) * (old_angle + math.radians(((old_y_gyro + y_gyro)* dt) / 2)) + k* y_angle)) #加速度とジャイロの相補フィルター
-    old_angle = y_angle #角度の変化量を足していくため一つ前の角度が必要
-    old_y_gyro = y_gyro #台形積分用に一つ前の角速度を残しておく
+    accel_only_angle = find_angle_in_acceleration(accel_x, accel_y, accel_z)
 
-    adjust_y_angle = 90-int(math.degrees(y_angle)) #初期位置は90度からなので
+    factor = complement_filter_coefficient(accel_x)
 
-    if adjust_y_angle < 180 and adjust_y_angle > 0: #センサーが0度~180度の時のみ動作
-        Servo.set_position(adjust_y_angle)
+    adjust_angle, prev_angle, prev_gyro = complement_filter(
+        accel_only_angle, gyro_y, prev_angle, prev_gyro, factor)
 
-    #計測時間がtotal_timeに達したらプログラム終了
-    if CALC_TIME < int(total_time):
+    if adjust_angle < 180 and adjust_angle > 0:  # 0度~180度の時のみ動作
+        servo.set_position(adjust_angle)
+
+    # 計測時間がcurrent_timeに達したらプログラム終了
+    if CALC_TIME < int(current_time):
         break
 
-    dt = time.time() - start #ループにかかる時間を微小時間dtに代入
-    total_time += dt
+    dt = time.time() - start  # ループにかかる時間を微小時間dtに代入
+    current_time += dt
 
-Servo.clean_up()
+servo.clean_up()
 
 with open('measurement.csv', 'w') as measurement_file:
     writer = csv.writer(measurement_file, lineterminator='\n')
     writer.writerow(fieldnames)
-    writer.writerows(data_logs)
+    writer.writerows(instrumentation_logs)
--- mpu6050.py
+++ mpu6050.py
@@ -13,6 +13,7 @@ GYRO_ZOUT = 0x47
 PWR_MGMT_1 = 0x6b
 PWR_MGMT_2 = 0x6c
 
+
 class Mpu6050:
 
     def __init__(self):
@@ -21,42 +22,41 @@ class Mpu6050:
 
     def __read_word(self, adr):
         high = self.bus.read_byte_data(DEV_ADDR, adr)
-        low  = self.bus.read_byte_data(DEV_ADDR, adr+1)
-        val  = (high << 8) + low
+        low = self.bus.read_byte_data(DEV_ADDR, adr+1)
+        val = (high << 8) + low
         return val
 
     def __read_word_sensor(self, adr):
-        val  = self.__read_word(adr)
+        val = self.__read_word(adr)
         if (val >= 0x8000):
             return -((65535 - val) + 1)
         else:
             return val
 
-    #角速度(rad/msec)取得
+    # 角速度(rad/msec)取得
     def __load_gyro_sensor(self):
-        x_gyro = self.__read_word_sensor(GYRO_XOUT)
-        y_gyro = self.__read_word_sensor(GYRO_YOUT)
-        z_gyro = self.__read_word_sensor(GYRO_ZOUT)
-        return [x_gyro, y_gyro, z_gyro]
+        gyro_x = self.__read_word_sensor(GYRO_XOUT)
+        gyro_y = self.__read_word_sensor(GYRO_YOUT)
+        gyro_z = self.__read_word_sensor(GYRO_ZOUT)
+        return [gyro_x, gyro_y, gyro_z]
 
     def gyro_lsb(self):
-        x_gyro,y_gyro,z_gyro = self.__load_gyro_sensor()
-        x_gyro /= 131
-        y_gyro /= 131
-        z_gyro /= 131
-        return [x_gyro, y_gyro, z_gyro]
+        gyro_x, gyro_y, gyro_z = self.__load_gyro_sensor()
+        gyro_x /= 131
+        gyro_y /= 131
+        gyro_z /= 131
+        return [gyro_x, gyro_y, gyro_z]
 
-    #加速度(m/msec)取得
+    # 加速度(m/msec)取得
     def __load_accelerometer(self):
-        x_accel = self.__read_word_sensor(ACCEL_XOUT)
-        y_accel = self.__read_word_sensor(ACCEL_YOUT)
-        z_accel = self.__read_word_sensor(ACCEL_ZOUT)
-        return [x_accel, y_accel, z_accel]
+        accel_x = self.__read_word_sensor(ACCEL_XOUT)
+        accel_y = self.__read_word_sensor(ACCEL_YOUT)
+        accel_z = self.__read_word_sensor(ACCEL_ZOUT)
+        return [accel_x, accel_y, accel_z]
 
     def accel_lsb(self):
-        x_accel,y_accel,z_accel = self.__load_accelerometer()
+        x_accel, y_accel, z_accel = self.__load_accelerometer()
         x_accel /= 16384
         y_accel /= 16384
         z_accel /= 16384
         return [x_accel, y_accel, z_accel]
-        
